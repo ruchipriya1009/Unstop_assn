9872f561adc0dea993927a5265e30ce5980da758
ruchi gupta <Ruchi Priya Kumari>
2023-07-26 15:53:21 +0530

    added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,9 +18,8 @@ import share from "./assest/share.png";
 import filter from "./assest/filter_list_alt.png";
 import search from "./assest/search.png";
 import barchart from "./assest/barchart.png";
-import Accordmobile from "./Accordmobile";
 
-import { Accordion, Box, Center, Flex, Text } from "@chakra-ui/react";
+import { Box, Center, Flex, Text } from "@chakra-ui/react";
 import Drawer from "./Drawer";
 import Mobile from "./Mobile";
 import Shwhide from "./Shwhide";
@@ -55,7 +54,7 @@ const Home = () => {
               <p>My Library</p>
             </div>{" "}
             <div>
-            <div className="hrs_line"></div>
+              <div className="hrs_line"></div>
               <img src={admin} />
             </div>{" "}
             <div>
@@ -109,7 +108,7 @@ const Home = () => {
 
                   {/* second                  */}
                   <div className="frame_sec4 sec5_rem">
-                    <Text color={"#1C4980"}>Candidates</Text>
+                    <Text color={"#1C4980"}>Condidates</Text>
 
                     <div className="frame_sec5">
                       <img src={condidate} />
@@ -135,7 +134,7 @@ const Home = () => {
                           fontWeight={"600"}
                           fontSize={"12px"}
                         >
-                          Total Candidate
+                          Total Condidate
                         </Text>
                       </Box>
                       <Box
@@ -288,14 +287,209 @@ const Home = () => {
 
               {/* ```````````````````````````````````````` */}
               <div>
-                <div>
-
-                </div>
+                <div></div>
                 {showMore && (
-                
-                <div>
-                  <Accordmobile/>
-                </div>
+                  <div>
+                    <div>
+                      <p className="asse">Assessments Overview</p>
+                      <div className="frame_sec12">
+                        {/* first part  */}
+                        <div className="frame_sec13">
+                          <div className="frame_sec4 first ">
+                            <Text fontSize={"14px"} color={"#1C4980"}>
+                              Total Assessment
+                            </Text>
+                            <div className="frame_sec5">
+                              <img src={total} />
+                              <Text
+                                fontSize={"20px"}
+                                fontWeight={700}
+                                fontFamily={"Inter"}
+                                color={"#1C4980"}
+                              >
+                                34
+                              </Text>
+                            </div>
+                          </div>
+
+                          <div className="frame_sec4 sec_rem4">
+                            <Text color={"#1C4980"}>Total Purpose</Text>
+                            <div className="frame_sec5">
+                              <img src={link} />
+                              <Text
+                                color={"#1C4980"}
+                                fontSize={"20px"}
+                                fontWeight={700}
+                                fontFamily={"Inter"}
+                              >
+                                11
+                              </Text>
+                            </div>
+                          </div>
+                        </div>
+                        {/* second                  */}
+                        <div className="frame_sec4 sec5_rem">
+                          <Text color={"#1C4980"}>Condidates</Text>
+
+                          <div className="frame_sec5">
+                            <img src={condidate} />
+                            <Box>
+                              <Flex alignItems={"center"} gap={"5px"}>
+                                <Text
+                                  color={"#1C4980"}
+                                  fontWeight={"700"}
+                                  fontSize={"20px"}
+                                >
+                                  11,145
+                                </Text>
+                                <Text
+                                  fontSize={"12px"}
+                                  color={"#05C165"}
+                                  fontWeight={"600"}
+                                >
+                                  +89
+                                </Text>{" "}
+                              </Flex>
+                              <Text
+                                color={"#1C4980"}
+                                fontWeight={"600"}
+                                fontSize={"12px"}
+                              >
+                                Total Condidate
+                              </Text>
+                            </Box>
+                            <Box
+                              w="1px"
+                              height={"40px"}
+                              border={"1px solid #DADCE0"}
+                            ></Box>
+                            <Box>
+                              <Flex alignItems={"center"} gap={"5px"}>
+                                <Text
+                                  color={"#1C4980"}
+                                  fontWeight={"700"}
+                                  fontSize={"20px"}
+                                >
+                                  1,14
+                                </Text>
+                                <Text
+                                  fontSize={"12px"}
+                                  color={"#05C165"}
+                                  fontWeight={"600"}
+                                >
+                                  +89
+                                </Text>{" "}
+                              </Flex>
+                              <Text
+                                color={"#1C4980"}
+                                fontWeight={"600"}
+                                fontSize={"12px"}
+                              >
+                                Who Attemped
+                              </Text>
+                            </Box>
+                          </div>
+                        </div>
+
+                        {/* Third part  */}
+                        <div className="frame_sec4 sec_rem3">
+                          <Text color={"#1C4980"}>Condidates Source</Text>
+
+                          <div className="frame_sec5">
+                            <img src={portal} />
+                            <Box>
+                              <Flex alignItems={"center"} gap={"5px"}>
+                                <Text
+                                  color={"#1C4980"}
+                                  fontWeight={"700"}
+                                  fontSize={"20px"}
+                                >
+                                  11,000
+                                </Text>
+                                <Text
+                                  fontSize={"12px"}
+                                  color={"#05C165"}
+                                  fontWeight={"600"}
+                                >
+                                  +89
+                                </Text>{" "}
+                              </Flex>
+                              <Text
+                                color={"#1C4980"}
+                                fontWeight={"600"}
+                                fontSize={"12px"}
+                              >
+                                E-mail
+                              </Text>
+                            </Box>
+                            <Box
+                              w="1px"
+                              height={"40px"}
+                              border={"1px solid #DADCE0"}
+                            ></Box>
+                            <Box>
+                              <Flex alignItems={"center"} gap={"5px"}>
+                                <Text
+                                  color={"#1C4980"}
+                                  fontWeight={"700"}
+                                  fontSize={"20px"}
+                                >
+                                  145
+                                </Text>
+                                <Text
+                                  fontSize={"12px"}
+                                  color={"#05C165"}
+                                  fontWeight={"600"}
+                                >
+                                  +89
+                                </Text>{" "}
+                              </Flex>
+                              <Text
+                                color={"#1C4980"}
+                                fontWeight={"600"}
+                                fontSize={"12px"}
+                              >
+                                Social Share
+                              </Text>
+                            </Box>
+                            <Box
+                              w="1px"
+                              height={"40px"}
+                              border={"1px solid #DADCE0"}
+                            ></Box>
+                            <Box>
+                              <Flex alignItems={"center"} gap={"5px"}>
+                                <Text
+                                  color={"#1C4980"}
+                                  fontWeight={"700"}
+                                  fontSize={"20px"}
+                                >
+                                  145
+                                </Text>
+                                <Text
+                                  fontSize={"12px"}
+                                  color={"#05C165"}
+                                  fontWeight={"600"}
+                                >
+                                  +89
+                                </Text>{" "}
+                              </Flex>
+                              <Text
+                                color={"#1C4980"}
+                                fontWeight={"600"}
+                                fontSize={"12px"}
+                              >
+                                Unique Link
+                              </Text>
+                            </Box>
+                          </div>
+                        </div>
+                        {/* forth part  */}
+
+                        {/* ending part  */}
+                      </div>
+                    </div>
+                  </div>
                 )}
                 {/* <img src={barchart} onClick={toggleShowMore} /> */}
               </div>
@@ -675,4 +869,3 @@ const Home = () => {
 };
 
 export default Home;
-
